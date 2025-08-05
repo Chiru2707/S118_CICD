5aeee53e19fcf0d9b324fdb9687259aa2b7ea035
Chiru2707 <chiranjeevimoguluri2@gmail.com>
2025-08-05 11:45:58 +0530

    Updated vite config and installed plugin

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 // vite.config.js
+// vite.config.js
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/S118_CICD/', // must match your GitHub repo name
   plugins: [react()],
-  base: '/S118_CICD/'
 });
+
